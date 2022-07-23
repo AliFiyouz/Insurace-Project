aa95a7a066c2ea3250f7f02cb13aa33746640e5d
AliFiyouz <Alifiyouz007@gmail.com>
2022-07-24 02:00:06 +0430

    firstSetup

---
+++ app.js
@@ -0,0 +1,17 @@
+let yearChoice = document.querySelector("#year-choice");
+let dateFormat = new Intl.DateTimeFormat("fa");
+
+// creat a cunstructor
+function YearCuns (Year){
+    this.Year = dateFormat.format(Date.now()).slice(0,4);
+    this.Year = dateFormat.format(Date.now()).slice(0,4);    
+}
+
+// creat 10 chioce for year selction and convert to english num
+for( let i =0 ;i<10;i++){
+    let option = document.createElement("option");
+    yearChoice.appendChild(option);
+    let english = new YearCuns().Year.replace(/۱۴۰۱/gi , "1401")
+    option.setAttribute("value", english );
+}
+
