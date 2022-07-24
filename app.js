3c83bd4fabc9b24dcc64d68159eae9e731e965ab
AliFiyouz <Alifiyouz007@gmail.com>
2022-07-24 08:40:06 +0430

    secund
    some css style
    js for year com

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ let dateFormat = new Intl.DateTimeFormat("fa");
 
 // creat a cunstructor
 function YearCuns (Year){
-    this.Year = dateFormat.format(Date.now()).slice(0,4);
-    this.Year = dateFormat.format(Date.now()).slice(0,4);    
+    this.Year = dateFormat.format(Date.now()).slice(0,4);   
 }
 
 // creat 10 chioce for year selction and convert to english num
-for( let i =0 ;i<10;i++){
+for( let i =0 ;i<21;i++){
     let option = document.createElement("option");
     yearChoice.appendChild(option);
-    let english = new YearCuns().Year.replace(/۱۴۰۱/gi , "1401")
-    option.setAttribute("value", english );
+    let english = new YearCuns().Year.replace(/۱۴۰۱/gi , 1401);
+    option.setAttribute("value", english-i );
 }
 
