7a7b5667e16c73495800ae7cd13504d2d2ca5f20
AliFiyouz <Alifiyouz007@gmail.com>
2022-07-26 09:28:15 +0430

    some litlle change on js

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ function YearCuns (Year){
     this.Year = dateFormat.format(Date.now()).slice(0,4);   
 }
 
+
+
 // creat 10 chioce for year selction and convert to english num
 for( let i =0 ;i<21;i++){
     let option = document.createElement("option");
